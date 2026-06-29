ebe7b00827646d07f5f8e81b602d80efdab0f4cf
cesar123easdwd <cesarliwag11@gmail.com>
2026-06-29 20:46:04 +0800

    first commit

---
+++ admin-dashboard/backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/database");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send(
+        "School Organization Admin API Running"
+    );
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(
+        `Server running on port ${PORT}`
+    );
+});
